24dbc8df7b5365b6eb9197f60fc8cfe0effc120e
Matthew Brunner <mpbrunner@protonmail.com>
2024-06-21 13:03:43 -0500

    Color changes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,8 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.color_scheme = 'Gruvbox Material (Gogh)'
 config.font = wezterm.font 'Hack Nerd Font'
 config.window_close_confirmation = 'NeverPrompt'
-config.window_background_opacity = 0.95
 config.enable_tab_bar = false
 config.enable_wayland = false
 return config
